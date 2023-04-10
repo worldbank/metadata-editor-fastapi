f69d3365827cd1721453d0de76d792a81684786e
Mehmood Asghar <mah0001@gmail.com>
2023-04-10 13:02:39 -0400

    misc

--- src/DataDictionary.py
+++ src/DataDictionary.py
@@ -2,6 +2,8 @@ class DataDictionary:
 
     def variable_decimal_percision(self, meta, variable_name):
         """Return the decimal percision for a variable in a dataframe"""
+
+        #return 0
         if meta.readstat_variable_types[variable_name] == 'double':
             return meta.original_variable_types[variable_name].split('.')[1].count('0')
         else:
