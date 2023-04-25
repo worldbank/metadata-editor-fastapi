33af7cba4d49e11c60d6d722257f953748ef155e
Mehmood Asghar <mah0001@gmail.com>
2023-04-25 16:34:57 -0400

    misc

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ import os
 from pydantic import BaseSettings
 import json
 from src.DictParams import DictParams
+import asyncio
+import functools
+import hashlib
+import datetime
+from fastapi.concurrency import run_in_threadpool
+import shutil
+import glob
+
+
 
 
 
@@ -45,6 +54,10 @@ class VarInfo(BaseModel):
 datadict=DataDictionary()
 
 app = FastAPI()
+app.fifo_queue = asyncio.Queue()
+
+app.jobs = {}
+
 
 
 @app.get("/")
@@ -78,13 +91,14 @@ async def data_dictionary_variable(params: DictParams):
     return datadict.get_data_dictionary_variable(params)
 
 
-        
-
-
-
 
 @app.post("/generate-csv")
 async def write_csv(fileinfo: FileInfo):
+    return write_csv_file(fileinfo)
+    
+
+
+def write_csv_file(fileinfo: FileInfo):
 
     file_ext=os.path.splitext(fileinfo.file_path)[1]    
     folder_path=os.path.dirname(fileinfo.file_path)
@@ -138,3 +152,134 @@ def detect_column_types(df,meta):
         df_types=df.convert_dtypes()
     
     return df_types.dtypes.to_dict()
+
+
+
+
+async def fifo_worker():
+    print("Starting FIFO worker")
+
+    # remove old jobs
+    remove_jobs_folder()
+
+    while True:
+        job = await app.fifo_queue.get()
+        print(f"Got a job: (size of remaining queue: {app.fifo_queue.qsize()})")
+        await job()
+
+
+
+@app.on_event("startup")
+async def start_queue():
+    asyncio.create_task(fifo_worker())        
+
+
+@app.post("/data-dictionary-queue")
+async def data_dictionary_queue(params: DictParams):
+    jobid='job-' + str(time.time())
+    app.jobs[jobid]={
+            "jobid":jobid,
+            "jobtype":"data-dictionary",
+            "status":"queued",
+            "info":params
+        }
+    
+    data_dict_callback = functools.partial(write_data_dictionary_file, jobid, params)
+    await app.fifo_queue.put( data_dict_callback )
+
+    return JSONResponse(status_code=202, content={
+        "message": "Item is queued",
+        "job_id": jobid
+        })
+
+
+
+@app.post("/generate-csv-queue")
+async def write_csv_queue(fileinfo: FileInfo):
+
+    jobid='job-' + str(time.time())
+    app.jobs[jobid]={
+            "jobid":jobid,
+            "jobtype":"generate-csv",
+            "status":"queued",
+            "info":fileinfo
+        }
+    
+    generate_csv_callback=functools.partial(write_csv_file_callback, jobid, fileinfo)
+    await app.fifo_queue.put( generate_csv_callback )
+
+    return JSONResponse(status_code=202, content={
+        "message": "file is queued",
+        "job_id": jobid
+        })
+
+    
+
+async def write_csv_file_callback(jobid, fileinfo: FileInfo):
+    loop = asyncio.get_running_loop()
+    app.jobs[jobid]["status"]="processing"
+
+    result=await loop.run_in_executor(None, write_csv_file, fileinfo)
+
+    app.jobs[jobid]["status"]="done"
+    file_path=os.path.join('jobs', str(jobid) + '.json')
+    with open(file_path, 'w') as outfile:
+        json.dump(result, outfile)
+        
+    return {"status": "success", "file_path": file_path}
+    
+    
+
+async def write_data_dictionary_file(jobid, params: DictParams):
+    loop = asyncio.get_running_loop()
+    datadict=DataDictionary()
+    app.jobs[jobid]["status"]="processing"
+
+    result=await loop.run_in_executor(None, datadict.get_data_dictionary_variable, params)
+
+    app.jobs[jobid]["status"]="done"
+    file_path=os.path.join('jobs', str(jobid) + '.json')
+    with open(file_path, 'w') as outfile:
+        json.dump(result, outfile)
+        
+    return {"status": "success", "file_path": file_path}
+
+
+
+@app.get("/jobs")
+async def queue_items():
+    return {
+            "queue_size": app.fifo_queue.qsize(),
+            "active_jobs": app.jobs
+            }
+
+
+@app.get("/jobs/{jobid}")
+async def queue_items(jobid: str):
+
+    if jobid in app.jobs:
+        job=app.jobs[jobid]
+        
+        if (job["status"]=="done"):
+            data={}
+            file_path=os.path.join('jobs', str(jobid) + '.json')
+            if os.path.exists(file_path):
+                with open(file_path) as json_file:
+                    data = json.load(json_file)
+            else:
+                raise HTTPException(status_code=400, detail="Failed to load job data") 
+
+            job['data']=data
+            return job 
+        else:
+            return job
+
+    raise HTTPException(status_code=404, detail="Job not found") 
+
+
+def remove_jobs_folder():
+    folder_path=os.path.join(os.getcwd(), 'jobs')
+    if os.path.exists(folder_path):        
+        files = glob.glob(folder_path + '/*.json')
+        for f in files:
+            os.remove(f)
